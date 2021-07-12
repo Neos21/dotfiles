0821b5a8c59852ff776bd1a02a2ea16f0496ef26
Neos21 <neos21@gmail.com>
2021-07-12 23:05:36 +0900

    Update Windows

---
+++ Windows/bin/gitbash.bat
@@ -0,0 +1,4 @@
+@Echo Off
+
+Rem GitBash を起動する
+Start C:\git-sdk-64\git-bash.exe
---
+++ Windows/bin/public-wsl-ip.ps1
@@ -0,0 +1,30 @@
+# Windows WSL2 に外部から直接アクセスできるようにする
+# https://rcmdnk.com/blog/2021/03/01/computer-windows-network/
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+$ip = bash.exe -c "ip r |tail -n1|cut -d ' ' -f9"
+if( ! $ip ){
+  echo "The Script Exited, the ip address of WSL 2 cannot be found";
+  exit;
+}
+
+# All the ports you want to forward separated by comma
+# 外部公開したいポート番号をカンマ区切りで列挙する
+$ports=@(22,3000,8080);
+$ports_a = $ports -join ",";
+
+# Remove Firewall Exception Rules
+iex "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' ";
+
+# Adding Exception Rules for inbound and outbound Rules
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+
+for( $i = 0; $i -lt $ports.length; $i++ ){
+  $port = $ports[$i];
+  iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=* connectport=$port connectaddress=$ip";
+}
+
+# Show proxies
+iex "netsh interface portproxy show v4tov4";
